d4ac2f89b1b539b286e194652a7645df77cae0b6
Vibhav <vibhavsingh9415@gmail.com>
2018-08-29 12:50:52 +0000

    User can add new campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 var express= require('express');
 var app= express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine","ejs");
+var campgrounds= [
+    {name: "Salmon Creek",image:"https://farm5.staticflickr.com/4117/4741325076_2d2b5d70dc.jpg"},
+    {name: "Mountain view",image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
+    {name: "Sierra Nevada",image:"https://farm9.staticflickr.com/8020/7538732802_49a42d28d2.jpg"}
+];
 
-app.listen(process.env.port,process.env.IP,function(){
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.get("/campgrounds",function(req,res){
+res.render("campgrounds",{campgrounds:campgrounds});
+    
+});
+    
+app.post("/campgrounds",function(req,res){
+    var name= req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name, image:image};
+    campgrounds.push(newCampground);
+    //redirect back to campgroundspage
+    res.redirect("/campgrounds");
+    
+});
+
+app.get("/campgrounds/new",function(req, res) {
+    res.render("new.ejs");
+});
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Yelp camp has started");
 });
